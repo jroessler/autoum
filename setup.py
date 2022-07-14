d91d46827eff411bbd12be7e3715f1e0cd211cf4
jroessler <roessler.jannik@gmx.de>
2022-07-14 21:32:25 +0200

    Bugfix setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ class PostInstallCommand(install):
         check_call("git clone https://github.com/jroessler/causalml.git".split(), cwd=root)
         check_call("python setup.py build_ext --inplace".split(), cwd=root + "/causalml")
         check_call("python setup.py install".split(), cwd=root + "/causalml")
-        check_call("pip install xbcausalforest==0.1.3".split())
 
 
 setup(name='autouplift',
