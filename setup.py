9c7e0d24f0a5a45a8f1cab0aa9a15762cc44fab3
jroessler <roessler.jannik@gmx.de>
2022-07-16 17:28:52 +0200

    Bugfix setup.py mac

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class PostInstallCommand(install):
         if platform == "linux" or platform == "linux2":
             check_call("python setup.py install".split(), cwd=root + "/causalml")
         else:
-            check_call("pip install causalml".split(), cwd=root + "/causalml")
+            check_call("pip install --no-cache-dir .".split(), cwd=root + "/causalml")
         check_call("pip install xbcausalforest==0.1.3".split())
 
 
